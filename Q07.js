db6dd215f327e65b74bd2201e18ed0501060b28e
mrrezaahmadi <mrrezaahmadi>
2020-04-12 17:02:32 +0430

    add solution of q07

--- Q07.js
+++ Q07.js
@@ -1,14 +1,8 @@
-// const mapper = (range1, range2, number) => {
-//     const ratio = (range1[1] - range1[0] + 1) / (range2[1] - range2[0] + 1)
-//     return number / ratio
-// }
-
-// console.log(mapper([1, 80], [30, 99.5], 3))
-
-function mapRange(baseRange = [1, 100], targetRange = [1, 5], num = 70) {
-    const ratio = (targetRange[1] - targetRange[0] + 1) / (baseRange[1] - baseRange[0] + 1);
-    console.log(ratio)
-    return (num - baseRange[0] + 1) * ratio + targetRange[0] - 1;
+const map = (range1, range2, number) => {
+    range2[0] -= 1
+    range1[0] -= 1
+    return range2[0] + ((number - range1[0]) * (range2[1] - range2[0])) / (range1[1] - range1[0])
 }
 
-console.log(mapRange([1, 100], [1, 5], 70))
+console.log(map([1, 80], [30, 99.5], 3))
+console.log(map([1, 100], [1, 5], 70))
