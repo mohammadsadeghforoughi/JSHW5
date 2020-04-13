0e2e42cdd8fa916edd5dbfd7fc5b26922825bd20
mrrezaahmadi <mrrezaahmadi>
2020-04-13 16:15:54 +0430

    fix unique number issue

--- Q05.js
+++ Q05.js
@@ -1,5 +1,33 @@
+let list = []
+
 const generateRandom = (start, end) => {
     return Math.floor(Math.random() * (end - start + 1) + start)
 }
 
-console.log(generateRandom(10000, 59999))
+const uniqueRandom = (start, end) => {
+    if (list.length === (end - start + 1)) {
+        return `More than range numbers!!!`
+    } else {
+        let random = generateRandom(start, end)
+        if (!list.includes(random)) {
+            list.push(random)
+        } else {
+            while (list.includes(random)) {
+                random = generateRandom(start, end)
+            }
+            list.push(random)
+        }
+        return random
+    }
+}
+
+console.log(uniqueRandom(1, 10))
+console.log(uniqueRandom(1, 10))
+console.log(uniqueRandom(1, 10))
+console.log(uniqueRandom(1, 10))
+console.log(uniqueRandom(1, 10))
+console.log(uniqueRandom(1, 10))
+console.log(uniqueRandom(1, 10))
+console.log(uniqueRandom(1, 10))
+console.log(uniqueRandom(1, 10))
+console.log(uniqueRandom(1, 10))
